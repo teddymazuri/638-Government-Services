96dec2e60c48ca2ae179516f0f1846070729f407
teddymazuri <50909066+teddymazuri@users.noreply.github.com>
2026-02-09 15:06:10 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ const CONFIG = {
                 <a class="navbar-brand" href="index.html">
                     <i class="fas fa-shield-alt me-2"></i>
                     <span class="brand-main">638</span>
-                    <span class="brand-accent">GOV</span>
+                    <span class="brand-accent">GOVERNMENT SERVICES</span>
                 </a>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-label="Toggle navigation">
